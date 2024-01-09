63495225f8558ccb57fa65bbd7b4a30522e7f6e8
Rahul Rasve <rahulrasve712@gmail.com>
2024-01-09 21:57:28 +0530

    all changes working

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import HomeBanner from '@/components/homepage/HomeBanner'
-import Recommendations from '@/components/homepage/Recommendations'
+import HomeBanner from "@/components/homepage/HomeBanner";
+import Recommendations from "@/components/homepage/Recommendations";
 
 export default function Home() {
-  return (
-    <main className='w-full bg-white min-h-screen'>
-      <HomeBanner />
-      
-    </main>
-  
-  )
+	return (
+		<main className="w-full bg-white min-h-screen">
+			<HomeBanner />
+			<Recommendations />
+		</main>
+	);
 }
